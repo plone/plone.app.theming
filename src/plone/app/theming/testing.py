b27c980b9fbc051cdd511722246ea3c2365c56b4
Eric BREHAULT <ebrehault@gmail.com>
2012-11-10 10:44:50 +0100

    those new gzip tests are actually NOT failling without the related fix in ThemeTransform.transformIterable, but there is not obvious way to reproduce the https://dev.plone.org/ticket/12038 bug with browser test (nor selenium by the way).

--- src/plone/app/theming/testing.py
+++ src/plone/app/theming/testing.py
@@ -22,6 +22,27 @@ class Theming(PloneSandboxLayer):
         # install into the Plone site
         applyProfile(portal, 'plone.app.theming:default')
 
+class ThemingWithCaching(Theming):
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # load ZCML
+        import plone.app.theming.tests
+        import plone.app.caching
+        xmlconfig.file('configure.zcml', plone.app.caching, context=configurationContext)
+        xmlconfig.file('configure.zcml', plone.app.theming.tests, context=configurationContext)
+
+        # Run the startup hook
+        from plone.app.theming.plugins.hooks import onStartup
+        onStartup(None)
+
+    def setUpPloneSite(self, portal):
+        # install into the Plone site
+        applyProfile(portal, 'plone.app.caching:default')
+        applyProfile(portal, 'plone.app.theming:default')
+
 THEMING_FIXTURE = Theming()
 THEMING_INTEGRATION_TESTING = IntegrationTesting(bases=(THEMING_FIXTURE,), name="Theming:Integration")
 THEMING_FUNCTIONAL_TESTING = FunctionalTesting(bases=(THEMING_FIXTURE,), name="Theming:Functional")
+THEMINGWITHCACHING_FIXTURE = ThemingWithCaching()
+THEMINGWITHCACHING_TESTING = IntegrationTesting(bases=(THEMINGWITHCACHING_FIXTURE,), name="Theming:IntegrationWithCaching")
