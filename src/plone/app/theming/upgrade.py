2cb8ca9d13b6973c640ddb9d1248e183495dafe7
Dan Jacka <danjacka@gmail.com>
2013-03-15 12:49:34 +1300

    Add upgrade step to combine p.a.theming and skins control panels
    The Theming control panels were combined in 6ee0b10aacca0672ace23027fcfd40a5280f3c86,
    but without an upgrade step. Added for those moving from 1.0 to
    1.1 (i.e. upgrading to Plone 4.3).

--- src/plone/app/theming/upgrade.py
+++ src/plone/app/theming/upgrade.py
@@ -6,3 +6,8 @@ def update_registry(context, logger=None):
     # Run the registry.xml step as that may have defined new attributes
     setup = getToolByName(context, 'portal_setup')
     setup.runImportStepFromProfile(PROFILE_ID, 'plone.app.registry')
+
+def update_controlpanel(context, logger=None):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'controlpanel',
+                                   run_dependencies=False)
