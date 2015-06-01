b3c191e821781beb652f743ee422735851c38695
Sam Schwartz <obct537@gmail.com>
2015-06-01 09:48:50 -0500

    Manually re-applying the theme overriding changes

--- src/plone/app/theming/utils.py
+++ src/plone/app/theming/utils.py
@@ -21,10 +21,11 @@ from plone.registry.interfaces import IRegistry
 from plone.resource.interfaces import IResourceDirectory
 from plone.resource.manifest import MANIFEST_FILENAME
 from plone.resource.manifest import extractManifestFromZipFile
-from plone.resource.manifest import getAllResources
 from plone.resource.manifest import getManifest
 from plone.resource.manifest import getZODBResources
+from plone.resource.manifest import getAllResources
 from plone.resource.utils import cloneResourceDirectory
+from plone.resource.utils import iterDirectoriesOfType
 from plone.resource.utils import queryResourceDirectory
 from plone.subrequest import subrequest
 from urlparse import urlsplit
@@ -61,7 +62,6 @@ class PythonResolver(etree.Resolver):
 
 def resolvePythonURL(url):
     """Resolve the python resource url to it's path
-
     This can resolve python://dotted.package.name/file/path URLs to paths.
     """
     assert url.lower().startswith('python://')
@@ -235,10 +235,8 @@ def isValidThemeDirectory(directory):
 
 def extractThemeInfo(zipfile, checkRules=True):
     """Return an ITheme based on the information in the given zipfile.
-
     Will throw a ValueError if the theme directory does not contain a single
     top level directory or the rules file cannot be found.
-
     Set checkRules=False to disable the rules check.
     """
 
@@ -330,13 +328,38 @@ def getTheme(name, manifest=None, resources=None):
 def getAvailableThemes():
     """Get a list of all ITheme's available in resource directories.
     """
-    resources = getAllResources(MANIFEST_FORMAT, filter=isValidThemeDirectory)
+    resources = getThemeResources(MANIFEST_FORMAT, filter=isValidThemeDirectory)
     themes = []
-    for name, manifest in resources.items():
-        themes.append(getTheme(name, manifest))
+    for theme in resources:
+        themes.append(getTheme(theme['name'], theme))
 
     themes.sort(key=lambda x: safe_unicode(x.title))
     return themes
+
+def getThemeResources(format, defaults=None, filter=None, manifestFilename=MANIFEST_FILENAME):
+
+    resources = []
+
+    for directory in iterDirectoriesOfType(format.resourceType, filter_duplicates=False):
+
+        if filter is not None and not filter(directory):
+            continue
+
+        name = directory.__name__
+
+        if directory.isFile(manifestFilename):
+
+            manifest = directory.openFile(manifestFilename)
+            try:
+                theme = getManifest(manifest, format, defaults)
+                theme['name'] = name
+                resources.append(theme)
+            except:
+                LOGGER.exception("Unable to read manifest for theme directory %s", name)
+            finally:
+                manifest.close()
+
+    return resources
 
 
 def getThemeFromResourceDirectory(resourceDirectory):
@@ -504,11 +527,6 @@ def createThemeFromTemplate(title, description, baseOn='template'):
         themeName = themeName.encode('utf-8')
 
     resources = getOrCreatePersistentResourceDirectory()
-    if themeName in resources:
-        idx = 1
-        while '-'.join((themeName, idx)) in resources:
-            idx += 1
-        themeName = '-'.join((themeName, idx))
 
     resources.makeDirectory(themeName)
     target = resources[themeName]
