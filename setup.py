933a8205a2c9a543089f50d748ce0f2d56e4519f
esteele <eric@esteele.net>
2015-09-20 21:17:18 +0200

    Back to development: 1.2.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-version = '1.2.13'
+version = '1.2.14.dev0'
 
 longdescription = open("README.rst").read()
 longdescription += "\n\n"
