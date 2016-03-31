27cd26fc50006e5b6186530de8bbde918965a9bf
esteele <eric@esteele.net>
2016-03-31 12:26:27 +0200

    Back to development: 1.2.20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-version = '1.2.19'
+version = '1.2.20.dev0'
 
 longdescription = open("README.rst").read()
 longdescription += "\n\n"
