0df5e342617fb3ff55c41269f6324ccede29cc42
Maurits van Rees <maurits@vanrees.org>
2016-08-11 23:57:10 +0200

    Preparing release 1.1.8
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.1.8 (unreleased)
+1.1.8 (2016-08-11)
 ------------------
 
 New features:
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1.8.dev0'
+version = '1.1.8'
 
 setup(name='plone.app.theming',
       version=version,
