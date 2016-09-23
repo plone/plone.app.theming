5e6456d3926df28dac2115362f2c1ead43ff3b0f
esteele <eric@esteele.net>
2016-09-23 15:07:05 -0400

    Back to development: 1.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-version = '1.3.2'
+version = '1.3.3.dev0'
 
 longdescription = open("README.rst").read()
 longdescription += "\n\n"
