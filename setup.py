adde765cd5d79fb634344119a62902544f6a2adc
Maurits van Rees <maurits@vanrees.org>
2016-12-02 19:59:19 +0100

    Back to development: 1.3.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-version = '1.3.3'
+version = '1.3.4.dev0'
 
 longdescription = open("README.rst").read()
 longdescription += "\n\n"
