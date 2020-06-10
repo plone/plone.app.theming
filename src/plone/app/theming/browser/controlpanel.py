9c64d46bf54ebf5259a8ccf400d329991a2cb70a
Maurits van Rees <maurits@vanrees.org>
2020-06-10 23:28:43 +0200

    Revert "fix hostnameBlacklist (Theming ControlPanel) in Py3"
    This reverts commit d6db78dbb930837c296aae87cea16bacde391a9b.
    
    No longer needed after calling processInputs.

--- src/plone/app/theming/browser/controlpanel.py
+++ src/plone/app/theming/browser/controlpanel.py
@@ -60,11 +60,6 @@ class ThemingControlpanel(BrowserView):
         """
         return getSite().absolute_url()
 
-    @property
-    def hostname_blacklist(self):
-        hostname_blacklist = self.request.get('hostnameBlacklist', [])
-        return [host.decode() for host in hostname_blacklist]
-
     def __call__(self):
         self.pskin = getToolByName(self.context, 'portal_skins')
         if self.update():
@@ -180,6 +175,8 @@ class ThemingControlpanel(BrowserView):
             prefix = form.get('absolutePrefix', None)
             doctype = str(form.get('doctype', ""))
 
+            hostnameBlacklist = form.get('hostnameBlacklist', [])
+
             parameterExpressions = {}
             parameterExpressionsList = form.get('parameterExpressions', [])
 
@@ -213,7 +210,7 @@ class ThemingControlpanel(BrowserView):
                 self.theme_settings.rules = rules
                 self.theme_settings.absolutePrefix = prefix
                 self.theme_settings.parameterExpressions = parameterExpressions
-                self.theme_settings.hostnameBlacklist = self.hostname_blacklist
+                self.theme_settings.hostnameBlacklist = hostnameBlacklist
                 if custom_css != self.theme_settings.custom_css:
                     self.theme_settings.custom_css_timestamp = datetime.now()
                 self.theme_settings.custom_css = custom_css
