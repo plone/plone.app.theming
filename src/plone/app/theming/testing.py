47a59fafdf5a4b5419485df70bb24f7a22bcd4b8
Jens W. Klein <jk@kleinundpartner.at>
2020-07-22 00:44:50 +0200

    Cleanup: Remove meanwhile unused test fixture code refering to plone.app.caching

--- src/plone/app/theming/testing.py
+++ src/plone/app/theming/testing.py
@@ -28,36 +28,6 @@ class Theming(PloneSandboxLayer):
         applyProfile(portal, 'plone.app.theming:default')
 
 
-class ThemingWithCaching(Theming):
-    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
-
-    def setUpZope(self, app, configurationContext):
-        # load ZCML
-        import plone.app.theming.tests
-        import plone.app.caching
-        xmlconfig.file(
-            'configure.zcml',
-            plone.app.caching,
-            context=configurationContext
-        )
-        xmlconfig.file(
-            'configure.zcml',
-            plone.app.theming.tests,
-            context=configurationContext
-        )
-
-        # Run the startup hook
-        from plone.app.theming.plugins.hooks import onStartup
-        onStartup(None)
-
-    def setUpPloneSite(self, portal):
-        # install into the Plone site
-        applyProfile(portal, 'plone.app.caching:default')
-        applyProfile(portal, 'plone.app.theming:default')
-        portal['portal_workflow'].setDefaultChain(
-            'simple_publication_workflow'
-        )
-
 THEMING_FIXTURE = Theming()
 THEMING_INTEGRATION_TESTING = IntegrationTesting(
     bases=(THEMING_FIXTURE,),
@@ -66,9 +36,4 @@ THEMING_INTEGRATION_TESTING = IntegrationTesting(
 THEMING_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(THEMING_FIXTURE,),
     name="Theming:Functional"
-)
-THEMINGWITHCACHING_FIXTURE = ThemingWithCaching()
-THEMINGWITHCACHING_TESTING = IntegrationTesting(
-    bases=(THEMINGWITHCACHING_FIXTURE,),
-    name="Theming:IntegrationWithCaching"
 )
