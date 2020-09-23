88d90a49188d4aba5b188093a47ac546e1ee9ed2
Maurits van Rees <maurits@vanrees.org>
2020-09-23 23:48:40 +0200

    Fixed WrongContainedType for hostnameBlackList on Zope 5.
    See also https://github.com/plone/plone.app.theming/issues/183.
    
    This reverts commit 9c64d46bf54ebf5259a8ccf400d329991a2cb70a
    which was itself a revert of "fix hostnameBlacklist (Theming ControlPanel) in Py3"
    
    In Plone 6 (with Zope 5) this is needed again, because `processInputs` no longer exists.

--- src/plone/app/theming/browser/controlpanel.py
+++ src/plone/app/theming/browser/controlpanel.py
@@ -60,6 +60,13 @@ class ThemingControlpanel(BrowserView):
         """
         return getSite().absolute_url()
 
+    @property
+    def hostname_blacklist(self):
+        hostname_blacklist = self.request.get('hostnameBlacklist', [])
+        if six.PY2:
+            return hostname_blacklist
+        return [host.decode() for host in hostname_blacklist]
+
     def __call__(self):
         self.pskin = getToolByName(self.context, 'portal_skins')
         if self.update():
@@ -175,8 +182,6 @@ class ThemingControlpanel(BrowserView):
             prefix = form.get('absolutePrefix', None)
             doctype = str(form.get('doctype', ""))
 
-            hostnameBlacklist = form.get('hostnameBlacklist', [])
-
             parameterExpressions = {}
             parameterExpressionsList = form.get('parameterExpressions', [])
 
@@ -210,7 +215,7 @@ class ThemingControlpanel(BrowserView):
                 self.theme_settings.rules = rules
                 self.theme_settings.absolutePrefix = prefix
                 self.theme_settings.parameterExpressions = parameterExpressions
-                self.theme_settings.hostnameBlacklist = hostnameBlacklist
+                self.theme_settings.hostnameBlacklist = self.hostname_blacklist
                 if custom_css != self.theme_settings.custom_css:
                     self.theme_settings.custom_css_timestamp = datetime.now()
                 self.theme_settings.custom_css = custom_css
