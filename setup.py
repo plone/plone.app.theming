5213d78b97435d9d2121b480ca4d74de3a7f0e8b
Gil Forcada Codinachs <gil.gnome@gmail.com>
2026-04-10 21:39:23 +0200

    chore: adjust trove classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords="plone diazo xdv deliverance theme transform xslt",
